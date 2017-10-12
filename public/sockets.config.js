c45784bed65a4c7d36735ef53c54ad13ddd95de5
Joshua Britz <joshuabritz@gmail.com>
2017-10-12 12:22:40 +0200

    Major UI changes

---
+++ public/sockets.config.js
@@ -0,0 +1,64 @@
+var nickname;
+var socket = io();
+$(function() {
+    nickname = getNickname(function(val) {
+        console.log('Connected as ' + val);
+    });
+
+    $('form').submit(function() {
+        socket.emit('chat message', { user: nickname, message: $('#m').val() });
+        $('#m').val('');
+        socket.emit('typing', '');
+        return false;
+    });
+    socket.on('chat message', function(msg) {
+        var messages = document.getElementById('messages');
+
+        var li = document.createElement('LI');
+        var b = document.createElement('B');
+        b.textContent = msg.user + ': ';
+        li.appendChild(b);
+        var span = document.createElement('SPAN');
+        span.textContent = msg.message;
+        li.appendChild(span);
+        var time = document.createElement('SPAN');
+        time.textContent = msg.timestamp;
+        time.classList.add('time');
+        li.appendChild(time);
+        messages.appendChild(li);
+    });
+});
+document.addEventListener('DOMContentLoaded', function() {
+    var m = document.getElementById('m');
+    m.addEventListener('keyup', function() {
+        if (m.value !== '') {
+            socket.emit('typing', nickname);
+        } else {
+            socket.emit('typing', '');
+        }
+    });
+    socket.on('typing', function(msg) {
+        var typing = document.querySelector('.typing');
+        if (msg) {
+            if (msg !== nickname) {
+                typing.innerHTML = msg + ' is typing';
+                typing.style.display = 'inline-flex';
+            }
+        } else {
+            typing.style.display = 'none';
+        }
+    });
+});
+
+function getNickname(cb) {
+    var localName = localStorage.getItem('username');
+    if (localName) {
+        cb(localName);
+        return localName;
+    } else {
+        var call = prompt('What is your name?');
+        localStorage.setItem('username', call);
+        cb(call);
+        return call;
+    }
+}
