eed873f348462c6f8d425f5a4e35d249ba5407e5
Joshua Britz <joshuabritz@gmail.com>
2017-10-12 09:45:08 +0200

    Render HTML

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-    res.send('Working');
+    res.sendFile(__dirname + '/index.html');
 });
 
 var port = process.env.PORT || 3124;
