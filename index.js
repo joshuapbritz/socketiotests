11fbda32e361f499229235ac60ed062863180fdd
Joshua Britz <joshuabritz@gmail.com>
2017-10-12 09:41:21 +0200

    Message

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var express = require('express');
 
 var app = express();
 
+app.get('/', (req, res) => {
+    res.send('Working');
+});
+
 var port = process.env.PORT || 3124;
 app.listen(port, () => {
     console.log(`Server started at - http://localhost:${port}`);
